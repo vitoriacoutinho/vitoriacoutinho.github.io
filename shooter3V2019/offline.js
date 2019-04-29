362f8e723f487e6a37d02a8b99c2b93d5d2116dd
vitoriacoutinho <50117541+vitoriacoutinho@users.noreply.github.com>
2019-04-29 15:34:49 -0300

    Add files via upload

---
+++ shooter3V2019/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1556562228,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/damage-sheet0.png",
+		"images/arrowup-sheet0.png",
+		"images/arrow2-sheet0.png",
+		"images/arrowleft-sheet0.png",
+		"images/arrowleft-sheet1.png",
+		"images/arrowdown-sheet0.png",
+		"images/arrowdown-sheet1.png",
+		"images/arrowright-sheet0.png",
+		"images/arrowright-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
